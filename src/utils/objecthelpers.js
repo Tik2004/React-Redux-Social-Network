ce0bb366fc2c2b2813ec4c9005d0c0386b20a4f6
Tiko <65791040+TikoA@users.noreply.github.com>
2020-11-22 12:26:42 +0400

    Update objecthelpers.js

--- src/utils/objecthelpers.js
+++ src/utils/objecthelpers.js
@@ -2,7 +2,7 @@
 export const updateObjectInArray = (items,ids,objPropname, newobjprops) => {
     return items.map(u => {
         if (u[objPropname] === ids) {
-            return {...u, ...newobjprops}
+            return { ...u, ...newobjprops }
         }
         return u;
     })
